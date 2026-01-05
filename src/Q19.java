8539c61fa682edd20b8b6271192c08ee03681234
Real1CM <liangchenmingg3er@Gmail.com>
2026-01-05 15:43:27 +0800

    Q19 二项图：二分和匈牙利算法

---
+++ src/Q19.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class Q19 {
+    static int N, M, K;
+    static int[][] matrix;
+    static int[] match;
+    static boolean[] vis;
+    static List<Integer>[] adj;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+
+        matrix = new int[N][M];
+        List<Integer> allValues = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                matrix[i][j] = sc.nextInt();
+                allValues.add(matrix[i][j]);
+            }
+        }
+
+        Collections.sort(allValues);
+
+        int left = 0;
+        int right = allValues.size() - 1;
+        int ans = allValues.get(right);
+
+        int targetMatch = N - K + 1;
+
+        while (left <= right) {
+            int midIdx = left + (right - left) / 2;
+            int midVal = allValues.get(midIdx);
+
+            if (isMatch(midVal, targetMatch)) {
+                right = midIdx - 1;
+                ans = midVal;
+            } else {
+                left = midIdx + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static boolean isMatch(int limit, int target) {
+        adj = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            adj[i] = new ArrayList<>();
+            for (int j = 0; j < M; j++) {
+                adj[i].add(matrix[i][j]);
+            }
+        }
+
+        int count = 0;
+        match = new int[M];
+        Arrays.fill(match, -1);
+
+        for (int i = 0; i < N; i++) {
+            vis = new boolean[M];
+            if (dfs(i)) {
+                count++;
+            }
+        }
+
+        return count >= target;
+    }
+
+    private static boolean dfs(int u) {
+        for (int v : adj[u]) {
+            if (!vis[v]) {
+                vis[v] = true;
+                if (match[v] == -1 || dfs(match[v])) {
+                    match[v] = u;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- src/ok.java
+++ src/ok.java
@@ -1,8 +1,35 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ok {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int targetFlaw = Integer.parseInt(sc.nextLine());
+        String str = sc.nextLine();
 
+        String vowels = "aeiouAEIOU";
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(vowels.indexOf(str.charAt(i)) != -1) {
+                indexes.add(i);
+            }
+        }
+
+        int left = 0;
+        int maxLen = 0;
+        for (int right = 0; right < indexes.size(); right++) {
+            int curFlaw = (indexes.get(right) - indexes.get(left)) +(right -left);
+
+            while (curFlaw > targetFlaw && right > left) {
+                left++;
+                curFlaw = (indexes.get(right) - indexes.get(left)) +(right -left);
+            }
+
+            if(targetFlaw == curFlaw) {
+                int curLen = indexes.get(right) - indexes.get(left);
+                maxLen = Math.max(maxLen, curLen);
+            }
+        }
     }
 }
